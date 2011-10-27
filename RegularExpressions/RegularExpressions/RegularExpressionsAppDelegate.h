722f13d73fcb5aafd04f85a251c8b9e77f923efa
Blackjacx <stefan.herold@gmail.com>
2011-10-27 21:51:38 +0200

    initial commit

---
+++ RegularExpressions/RegularExpressions/RegularExpressionsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RESTOperationsAppDelegate.h
+//  RESTOperations
+//
+//  Created by *** *** on 9/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegularExpressionsAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
