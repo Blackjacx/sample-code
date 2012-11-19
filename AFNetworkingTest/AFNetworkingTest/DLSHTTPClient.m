1c39b7bcae9d522f3a408c368bc46f1920c61d2b
Blackjacx <stefan.herold@gmail.com>
2012-11-19 19:54:01 +0100

    * added frameworks

--- AFNetworkingTest/AFNetworkingTest/DLSHTTPClient.m
+++ AFNetworkingTest/AFNetworkingTest/DLSHTTPClient.m
@@ -7,7 +7,45 @@
 //
 
 #import "DLSHTTPClient.h"
+#import "AFHTTPClient.h"
+#import "AFJSONRequestOperation.h"
+
+@interface DLSHTTPClient ()
+
+@property(nonatomic, strong)AFHTTPClient * httpClient;
+
+@end
 
 @implementation DLSHTTPClient
+
+// MARK: Init
+
+- (DLSHTTPClient *)sharedInstance {
+
+	static DLSHTTPClient * instance = nil;
+	static dispatch_once_t predicate;
+
+	dispatch_once(&predicate, ^{
+
+		instance = [[DLSHTTPClient alloc] init];
+	});
+	return instance;
+}
+
+- (id)init {
+
+	self = [super init];
+
+	if( self ) {
+
+		NSURL * baseURL = [NSURL URLWithString:@""];
+		_httpClient = [[AFHTTPClient alloc] initWithBaseURL:baseURL];
+	}
+	return self;
+}
+
+// MARK: Discovery Information
+
+
 
 @end
