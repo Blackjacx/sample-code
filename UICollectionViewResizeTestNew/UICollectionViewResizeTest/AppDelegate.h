a764e5efbce22e1d50d8ee20299d107249e060fa
Stefan Herold <stefan.herold@gmail.com>
2020-02-12 10:06:40 +0100

    Re add project

---
+++ UICollectionViewResizeTestNew/UICollectionViewResizeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewResizeTest
+//
+//  Created by Stefan Herold on 13/05/15.
+//  Copyright (c) 2015 Stefan Herold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
